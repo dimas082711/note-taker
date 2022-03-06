ed239cf0c705ea3eccfb5df8d1c91c09deee1fde
dimas <dprcontractor@gmail.com>
2022-03-06 12:49:25 -0500

    setup api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+
+
+router.get('./notes', (req, res) => {
+    database = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    res.json(database);
+});
+
+router.post('/notes', (req, res) => {
+    let noteModel = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.random() * 5000),
+    };
+    database.push(noteModel);
+    fs.writeFileSync('./db/db.json', JSON.stringify(database), function (err) {
+        if (err) throw err;
+    });
+    res.json(database);
+});
+
+router.delete('/notes/:id', function (req, res) {
+    
+
+let existNotes = [];
+
+for (let i = 0; i < database.length; i++) {
+    if (database[i].id != req.params.id) {
+        existNotes.push(database[i]);
+    }
+}
+database = existNotes;
+
+fs.writeFileSync(
+    './db/db.json',
+    JSON.stringify(database),
+    function (err, res) {
+        if (err) throw (err);
+    }
+);
+res.json(database);
+})
+
+module.exports = router;
